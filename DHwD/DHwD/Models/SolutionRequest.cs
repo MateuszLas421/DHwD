08be760321b4eb02e3655575f06dae8575f9496c
MateuszLas421 <mateuszlas3@gmail.com>
2021-03-16 23:16:40 +0100

    add SolutionRequest

---
+++ DHwD/DHwD/Models/SolutionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DHwD.Models
+{
+    public class SolutionRequest
+    {
+        public int idMystery { get; private set; }
+
+        public string TextSolution { get; private set; }
+
+        public DateTime dataTimeRequest { get; set; }
+
+    }
+}
