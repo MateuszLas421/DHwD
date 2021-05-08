e2b541f7ee0bba0d3f9d7c5709611e13d6b44f4f
Mateusz Łaś <mateuszlas3@gmail.com>
2021-05-08 16:22:21 +0200

    PrepareGetRequest

--- DHwD/DHwD/Service/Tools/PrepareGetRequest.cs
+++ DHwD/DHwD/Service/Tools/PrepareGetRequest.cs
@@ -7,10 +7,14 @@ namespace Models.Request
 {
     public static class PrepareGetRequest
     {
-
         public static Task<GetRequest> PrepareFirstParametr(GetRequest getRequest, string nameparametr, string parametr)
         {
             getRequest.strURL += nameparametr + "=" + parametr;
+            return Task.FromResult<GetRequest>(getRequest);
+        }
+        public static Task<GetRequest> PrepareMoreParametr(GetRequest getRequest, string nameparametr, string parametr)
+        {
+            getRequest.strURL += ""+ nameparametr + "=" + parametr;
             return Task.FromResult<GetRequest>(getRequest);
         }
     }
